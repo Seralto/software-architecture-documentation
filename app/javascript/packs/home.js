e5884e558da11d06aabedc35d8129a7995f98586
Sérgio Toledo <contato@sergiotoledo.com.br>
2020-08-22 23:27:43 -0300

    Better entity styles

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,3 +1,4 @@
 import '../src/home/components/tree.scss';
+import '../src/home/components/entity.scss';
 
 import {} from 'src/home/index';
--- app/javascript/src/home/components/hierarchy_item.js
+++ app/javascript/src/home/components/hierarchy_item.js
@@ -3,9 +3,31 @@ import React from 'react';
 const HierarchyItem = props => {
   const { item } = props;
 
+  const capitalize = name => {
+    return name.charAt(0).toUpperCase() + name.slice(1)
+  }
+
+  const buildMethodsList = (type, list) => {
+    if (!list || list.length === 0) { return };
+
+    return (
+      <div className='methods-box'>
+        <p className='method-type'><span>{capitalize(type)} methods</span></p>
+        {list.map((method) => { return <span key={method} className='method-name'>{`${method}()`}</span> })}
+      </div>
+    )
+  };
+
   return (
     <div className='entity-box'>
-      {item.name}
+      <div className='entity-box-header'>
+        {item.name}
+      </div>
+
+      <div className='entity-box-body'>
+        {buildMethodsList('public', item.public_methods)}
+        {buildMethodsList('class', item.class_methods)}
+      </div>
     </div>
   );
 }
